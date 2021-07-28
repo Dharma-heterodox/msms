a8a41a3625546f97c5bbb4be69016bce58dad8ff
Dharma-heterodox <dharma.rvs2005@gmail.com>
2021-07-28 08:20:27 +0530

    Username log debug

--- src/main/java/com/school/core/filter/JwtRequestFilter.java
+++ src/main/java/com/school/core/filter/JwtRequestFilter.java
@@ -50,7 +50,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		} else {
 			logger.warn("JWT Token does not begin with Bearer String");
 		}
-
+		logger.debug("Logged-in username :::: "+username);
 		//Once we get the token validate it.
 		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
